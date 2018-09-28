d75f76a836ed6d76a12e507f5feb842d5c430aa2
KouYidong <1223519172@qq.com>
2018-09-28 19:07:13 +0800

    临时存储

--- 数字输入框组件/input-number.js
+++ 数字输入框组件/input-number.js
@@ -1,37 +1,48 @@
 Vue.component('input-number', {
-    template: `
+  template: `
         <div class="input-number">
             <input type="text" :value="num" />
         </div>
     `,
-    props: {
-        max: {
-            type: Number,
-            default: Infinity // 无限大
-        },
-        min: {
-            type: Number,
-            default: -Infinity // 无限小
-        },
-        value: {
-            type: Number,
-            default: 0
-        }
+  props: {
+    max: {
+      type: Number,
+      default: Infinity // 无限大
     },
-    // data() {
-    //     return {
-    //         currentValue: this.value // 初始化时引用父组件的value
-    //     }
-    // },
-    // 大多数下建议用computed
-    // watch: {
-    //     currentValue(val) {
-    //         this.$emit('input', val);
-    //     }
-    // }
-    computed: {
-        num() {
-            return this.value
-        },
+    min: {
+      type: Number,
+      default: -Infinity // 无限小
+    },
+    value: {
+      type: Number,
+      default: 0
     }
+  },
+  data() {
+    return {
+      currentValue: this.value // 初始化时引用父组件的value
+    }
+  },
+  methods: {
+    updateValue(val) {
+      if (val > this.max) {
+        val = this.max;
+      }
+      if (val < this.min) {
+        val = this.min;
+      }
+      this.currentValue = val;
+    }
+  },
+  computed: {
+    num() {
+      return this.updateValue(this.value);
+    }
+  },
+  watch: {
+    // watch监听的回调函数中有两个参数，第一个是新的值，
+    currentValue(val) {
+      this.$emit('input', val);
+    }
+  }
 });
