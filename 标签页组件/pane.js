9bed62e60ddb35d4d6226c26344fc963e544f3f4
KouYidong <1223519172@qq.com>
2018-09-29 20:42:58 +0800

    增加标签页组件

---
+++ 标签页组件/pane.js
@@ -0,0 +1,46 @@
+Vue.component('pane', {
+  name: 'pane',
+  template: `
+    <div class="pane" v-show="isShow">
+      <slot></slot>
+    </div>`,
+  props: {
+    // 用户可选的设置唯一标识，默认从0开始
+    name: {
+      type: String
+    },
+    // 标签页的标题
+    label: {
+      type: String,
+      default: ''
+    },
+    // 可否关闭
+    closable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      // 用来控制对应标签页的显示和隐藏
+      isShow: true
+    }
+  },
+  methods: {
+    updateNav() {
+      // 更新父组件
+      this.$parent.updateNav();
+    }
+  },
+  watch: {
+    label() {
+      this.updateNav();
+    },
+    closable() {
+      this.updateNav();
+    }
+  },
+  mounted() {
+    this.updateNav();
+  }
+});
